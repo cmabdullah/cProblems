2cc22faa93c0d8ddef061957cb550bee2f8a8f9d
cmabdullah <a.kium.khan@gmail.com>
2016-10-01 23:25:19 +0600

    solve chapter 2

---
+++ Chapter2/2.7.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int x, y, z ;
+    x =100100;
+    y =200200;
+    z = x + y;
+
+    printf("%d\n%d\n\n%d", x, y,z);
+}
---
+++ Chapter2/2.9.c
@@ -0,0 +1,17 @@
+// Structure using typedef:
+
+#include <stdio.h>
+#include <string.h>
+
+
+
+
+int main()
+{
+  typedef float student;
+
+  student x;
+  x = 10;
+  printf("%f",x);
+
+}
