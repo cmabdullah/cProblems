d8f2cc8cc21906ca0f9766cd75da14f1ca472e7c
cmandullah <a.kium.khan@gmail.com>
2016-12-23 19:42:31 -0800

    add some variables like as register, static , external , and some extinsiv tools

---
+++ Chapter9/examp/9.16Recursion_page-295ANSI.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+// if you learn more see recursion.docx on this repository
+main(){
+    printf("This is an example of recursion\n");
+    main();
+}
---
+++ Chapter9/examp/9.5program_page_291ANSI.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main(){
+    int x, y ;
+    double power(int , int);
+    printf("enter x ,  y:");
+    scanf("%d%d", &x, &y) ;
+    printf("%d to power %d is %f", x,y, power(x,y));
+}
+
+double power(int x, int y){
+    double p;
+    p = 1.0;
+    if (y  >= 0)
+        while (y--)
+        p *= x;
+    else
+        while(y++)
+        p /= x;
+    return (p);
+
+
+}
